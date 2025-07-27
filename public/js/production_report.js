3502d9c0b2bbea952e02984ca9d40f239347bf7b
Gauresh <gchari14@gmail.com>
2025-07-28 00:48:48 +0530

    Remove all files from Git tracking - fresh start

--- public/js/production_report.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const backBtn = document.querySelector('.header-back-button');
-    if (backBtn) {
-        backBtn.addEventListener('click', function() {
-            window.location.href = 'employee_dashboard.html';
-        });
-    }
-}); 
